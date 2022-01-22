a7fe4fdabad56efe0b3953afa7d877452ad51f1f
Anas Basheer <nasbas23@gmail.com>
2022-01-22 14:55:12 +0300

    Added functionality to enter and clear text

--- script.js
+++ script.js
@@ -25,3 +25,44 @@ const operate = (a, b, operator) => {
                     ? divide(a, b)
                     : null;
 }
+
+const display = document.querySelector('.row.input .col');
+let displayValue = display.textContent;
+
+const digitBtns = document.querySelectorAll('button.digit');
+
+digitBtns.forEach(button => {
+    button.addEventListener('click', (event) => {
+        const value = event.target.id;
+        display.textContent = displayValue.length <= 16
+            ? value !== 'point'
+                ? displayValue === '0'
+                    ? value
+                    : displayValue + value
+                : displayValue.includes('.')
+                    ? displayValue
+                    : displayValue + '.'
+            : displayValue;
+        displayValue = display.textContent;
+    })
+})
+
+const allClearBtn = document.querySelector('#all-clear');
+allClearBtn.addEventListener('click', () => {
+    display.textContent = '0';
+    displayValue = display.textContent;
+})
+
+const clearEntryBtn = document.querySelector('#clear-entry');
+clearEntryBtn.addEventListener('click', () => {
+    display.textContent = '0';
+    displayValue = display.textContent;
+})
+
+const backspaceBtn = document.querySelector('#backspace');
+backspaceBtn.addEventListener('click', () => {
+    display.textContent = displayValue.length === 1
+        ? '0'
+        : displayValue.slice(0, -1);
+    displayValue = display.textContent;
+})
