2278eb79c20afb70c2478d41d52eda0cd6143e5e
Anas Basheer <nasbas23@gmail.com>
2022-01-28 07:13:41 +0300

    Now we can get result after an operation of 2 numbers

--- script.js
+++ script.js
@@ -24,45 +24,134 @@ const operate = (a, b, operator) => {
                 : operator === 'divide'
                     ? divide(a, b)
                     : null;
-}
+};
+
+// Storage will have at most three properties,
+// 2 of them are the operands and the 3rd one is the operator
+let storage = {};
 
 const display = document.querySelector('.row.input .col');
-let displayValue = display.textContent;
 
-const digitBtns = document.querySelectorAll('button.digit');
+// Will return the display content (ie whatever numbers have been inputted)
+const getDisplayValue = () => display.textContent;
 
-digitBtns.forEach(button => {
-    button.addEventListener('click', (event) => {
-        const value = event.target.id;
-        display.textContent = displayValue.length <= 16
-            ? value !== 'point'
-                ? displayValue === '0'
-                    ? value
-                    : displayValue + value
-                : displayValue.includes('.')
-                    ? displayValue
-                    : displayValue + '.'
-            : displayValue;
-        displayValue = display.textContent;
-    })
-})
+const setDisplayValue = (value) => {
+    console.log(value.toString().length);
 
-const allClearBtn = document.querySelector('#all-clear');
-allClearBtn.addEventListener('click', () => {
-    display.textContent = '0';
-    displayValue = display.textContent;
-})
+    if (value.toString().length > 16) {
+        display.textContent = (+value).toExponential(10);
+    } else {
+        display.textContent = value;
+    }
+};
 
-const clearEntryBtn = document.querySelector('#clear-entry');
-clearEntryBtn.addEventListener('click', () => {
-    display.textContent = '0';
-    displayValue = display.textContent;
-})
+const digitPressed = (event) => {
+    // function to be run when a digit button or a decimal point button is pressed
 
-const backspaceBtn = document.querySelector('#backspace');
-backspaceBtn.addEventListener('click', () => {
-    display.textContent = displayValue.length === 1
-        ? '0'
-        : displayValue.slice(0, -1);
-    displayValue = display.textContent;
-})
+    const digit = event.target.id;  // The number being pressed
+
+    // if operator is set then reset display
+    if (!storage.operatorPressChecked) {
+        setDisplayValue('0');
+        storage.operatorPressChecked = true;
+    }
+
+    setDisplayValue(
+        getDisplayValue().length <= 16 // Cannot input more that 17 characters
+            ? digit !== 'point'   // only if the pressed button is not a decimal point
+
+                ? getDisplayValue() === '0'
+                    ? digit // If the display is 0 then change to the inputted number
+                    : getDisplayValue() + digit
+
+                : getDisplayValue().includes('.')
+                    ? getDisplayValue()  // if it already has a point in it, then dont do anything
+                    : getDisplayValue() + '.'
+
+            : getDisplayValue() // when the length is more than 17, dont do anything
+    );
+};
+
+const clearDisplay = (event) => {
+    // all-clear: clears everything that is done so far, clear, the operands and operator
+    // clear-entry: clears the current display so that it can be typed again, similar to backspacing until 0
+
+    const clearType = event.target.id; // either all-clear or clear-entry
+
+    if (clearType === 'backspace') {
+        backspaceDisplay();
+        return;
+    }
+
+    setDisplayValue('0');
+    if (clearType === 'all-clear') {
+        // delete everything in storage
+        for (let prop in storage) {
+            delete storage[prop];
+        }
+    }
+};
+
+const backspaceDisplay = () => {
+    setDisplayValue(
+        getDisplayValue().length === 1
+            ? '0'
+            : getDisplayValue().slice(0, -1)
+    );
+};
+
+const equalPressed = () => {
+    if (!("operand1" in storage)) {
+        setDisplayValue('0');
+        return;
+    }
+
+    const operand1 = storage.operand1;
+
+    // const operand2 = ("operator" in storage) ? +getDisplayValue() : storage.operand2;
+    let operand2;
+    let operatorInvolved;
+    if ("operator" in storage) {
+        operatorInvolved = storage.operator;
+        operand2 = +getDisplayValue();
+    } else if ("cachedOperator" in storage) {
+        operatorInvolved = storage.cachedOperator;
+        operand2 = storage.operand2;
+    } else {
+        return;
+    }
+
+    const result = operate(operand1, operand2, operatorInvolved);
+
+    setDisplayValue(result.toString());
+
+    storage.operand1 = result;
+    storage.operand2 = operand2;
+
+    storage.cachedOperator = operatorInvolved;
+    delete storage.operator;
+}
+
+const operatorPressed = (event) => {
+    // function that runs when an operator is pressed including equal sign
+
+    const operatorPressed = event.target.id;
+
+    if (operatorPressed === 'equal') {
+        equalPressed();
+    } else {
+        storage.operand1 = +getDisplayValue();
+        storage.operator = operatorPressed;
+        storage.operatorPressChecked = false;
+    }
+};
+
+
+const pressedBtnOfType = (className, func) => {
+    const typeBtns = document.querySelectorAll(className);
+    typeBtns.forEach(button => button.addEventListener('click', func));
+};
+
+pressedBtnOfType('.digit', digitPressed);
+pressedBtnOfType('.misc', clearDisplay);
+pressedBtnOfType('.operator', operatorPressed);
